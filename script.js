a02844a9d46924f31de717146ccf8f0dcda8e5bc
Gaurav Singh <gauravnumber@gmail.com>
2024-02-23 13:20:57 +0530

    improve little

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ function mapOfURL(sitename, yourSearch) {
 
   sites.set("kdrama", `https://runasian.net/search?keyword=${yourSearch}`);
   sites.set("movie", `https://moviesmod.biz/search/${yourSearch}`);
+  sites.set("my-drama-list", `https://mydramalist.com/search?q=${yourSearch}`);
+  sites.set(
+    "my-anime-list",
+    `https://myanimelist.net/anime.php?q=${yourSearch}&cat=anime`
+  );
+  sites.set(
+    "anime-planet",
+    `https://www.anime-planet.com/anime/all?name=${yourSearch}`
+  );
+  sites.set("truecaller", `https://www.truecaller.com/search/in/${yourSearch}`);
+  sites.set(
+    "annas-archive",
+    `https://annas-archive.org/search?index=&q=${yourSearch}&sort=&lang=en`
+  );
+  sites.set(
+    "pdfdrive",
+    `https://www.pdfdrive.com/search?q=${yourSearch}&pagecount=&pubyear=&searchin=en`
+  );
 
   const url = sites.get(sitename);
 
