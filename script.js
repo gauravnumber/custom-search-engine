dc50130a762671476ef10c41c7d36b0722e8dfd3
Gaurav Singh <gauravnumber@gmail.com>
2024-03-17 14:44:55 +0530

    bollywood link added

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function mapOfURL(sitename, yourSearch) {
   );
 
   sites.set("kdrama", `https://runasian.net/search?keyword=${yourSearch}`);
+  sites.set("bollywood", `https://topmovies.asia/search/${yourSearch}`);
   sites.set("movie", `https://moviesmod.biz/search/${yourSearch}`);
   sites.set("my-drama-list", `https://mydramalist.com/search?q=${yourSearch}`);
   sites.set(
